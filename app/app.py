10a425f27848380841d639bed5c0040f48de335d
Rishabh Raj <rishabhraj.891@gmail.com>
2026-01-01 15:07:04 +0530

    project setup

---
+++ app/app.py
@@ -0,0 +1,115 @@
+import streamlit as st
+from helper import get_api_keys, encode_image, extract_with_openai, update_gsheet,get_gsheet_data
+from PIL import Image
+import pandas as pd
+import json
+import os
+from datetime import datetime
+
+
+
+st.set_page_config(page_title="SARAL OCR", layout="wide")
+
+
+openai_api = get_api_keys()
+
+# Initialize state
+if "invoice_data" not in st.session_state:
+    st.session_state.invoice_data = None
+    st.session_state.model = None
+if "refresh_gsheet" not in st.session_state:
+    st.session_state.refresh_gsheet = False
+
+with st.sidebar:
+    st.title("SARAL OCR")
+
+    model_name = st.selectbox(
+        "Select Model",
+        ["gpt-3.5-turbo", "gpt-4", "gpt-4o", "gpt-4o-mini", "gpt-4.1","gpt-4.1-mini"],
+        index=0,
+    )
+
+    st.subheader("📤 Upload Invoice")
+
+    uploaded_file = st.file_uploader(
+        "Choose an invoice image",
+        type=["png", "jpg", "jpeg"],  # removed pdf (important)
+        help="Upload a clear image of your invoice",
+    )
+
+    if uploaded_file:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Invoice",width='400')
+        original_filename = uploaded_file.name
+        image_name = os.path.splitext(original_filename)[0]
+
+        if st.button("Extract Invoice",width='400'):
+            with st.spinner("Extracting Invoice..."):
+                try:
+                    uploaded_file.seek(0)
+                    image_base64 = encode_image(uploaded_file)
+
+                    st.session_state.invoice_data, st.session_state.model = extract_with_openai(
+                        image_base64, model_name, openai_api
+                    )
+
+                except Exception as e:
+                    st.error(f"Error extracting invoice: {str(e)}")
+
+# ------------------ RESULT ------------------
+
+if st.session_state.invoice_data and st.session_state.model:
+  
+
+    raw = st.session_state.invoice_data
+
+    # Safe JSON extraction
+    try:
+        # First try to load it directly
+        json_data = raw
+        # If it contains markdown code blocks, strip them
+        if "```json" in raw:
+            json_data = raw.split("```json")[1].split("```")[0]
+        elif "```" in raw:
+            json_data = raw.split("```")[1].split("```")[0]
+            
+        # Parse JSON
+
+        parsed = json.loads(json_data)
+
+        data_dict = {
+            "id": image_name,
+            **parsed,
+            "model_name": st.session_state.model,
+            "scan_timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        }
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader("Invoice Data")
+        with col2:
+            if st.button("Save to Google Sheets"):
+                with st.spinner("Saving to Google Sheets..."):
+                    try:
+                        update_gsheet(data_dict)
+                        st.session_state.refresh_gsheet = True
+                        st.rerun()
+                        st.success("✅ Data successfully saved to Google Sheets!")
+                    except Exception as e:
+                        st.error(f"Error saving to Google Sheets: {e}")
+
+        st.json(data_dict)
+        if st.session_state.refresh_gsheet:
+            sheet_df = get_gsheet_data()
+            st.session_state.refresh_gsheet = False
+        else:
+            sheet_df = get_gsheet_data()
+
+        st.subheader("📊 Google Sheets Data")
+        st.dataframe(sheet_df, use_container_width=True)        
+       
+    except json.JSONDecodeError:
+        st.error("Failed to parse JSON")
+        st.code(raw)
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+        st.code(raw)
---
+++ app/helper.py
@@ -0,0 +1,202 @@
+import streamlit as st
+import openai
+import base64
+import json
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
+INVOICE_SCHEMA={
+    "invoice_number": "",
+    "invoice_date": "",
+    "invoice_type" : "Tax Invoice | Credit Note | Or anything else",
+    "grand_total": 'The final payable amount',
+    "seller_name" : "Name of the vendor",
+    "buyer_name" : "Name of the buyer",
+"total_taxable_value":"Base amount before tax",
+"total_tax_amount": "Total GST amount",
+"currency_code": "USD, INR, GB  P, JPY, AED etc",
+"purchase_order_numbers": "Comma-separated list (e.g., 'PO-01, PO-02').",
+"delivery_challan_numbers" : "Comma-separated list of Challans/GRNs.",
+"line_items": [
+    {
+    "line_number": "Sequential ID (1, 2...)",
+    "description": "Item description.",
+    "classification":{
+        "hsn_sac_code": " GST Classification Code (Critical)",
+        "is_service": "True for Services, False for Goods"
+    },
+    "quantity": {
+        "billed_quantity":"Quantity invoiced",
+        "unit_of_measure": " Standard UOM (NOS, KGS, MTR)"
+    },
+    'financial':{
+"unit_price": "Rate per unit",
+"gross_amount": "Qty * Price",
+"discount_amount" : "Line level discount",
+"taxable_value" : "Base value for tax calculation",
+    },
+    'taxes':{
+        "tax_rate_percentage" : "5, 12, 18, or 28",
+        "igst_amount": "Integrated GST",
+        "cgst_amount" : "Central GST",
+        "sgst_amount" : "State GST"
+    },
+    "total_line_amount" : "Final line value"
+}],
+'seller_details': {
+    "name" : " Legal Name",
+    "gstin" : "15-digit Tax ID (Validated Regex)",
+    "pan" : "10-digit PAN",
+    "state_code" : "2-digit GST State Code (e.g., '27').",
+    "address":{
+"street" : "",
+"city" : "",
+"state" : "",
+"pincode" : "",
+"country" : "",
+    },
+    "email": "",
+    "phone": "",
+    'bank_details' : {
+        'account_number': "",
+        'ifsc_code': "",
+        'bank_name': "",
+    }
+},
+'buyer_details': {
+    "name" : " Legal Name",
+    "gstin" : "15-digit Tax ID (Validated Regex)",
+    "pan" : "10-digit PAN",
+    "state_code" : "2-digit GST State Code (e.g., '27').",
+    "address":{
+"street" : "",
+"city" : "",
+"state" : "",
+"pincode" : "",
+"country" : "",
+    },
+    "email": "",
+    "phone": ""
+},
+'financial_totals':{
+'total_taxable_value' : 'Decimal value',
+'total_tax_amount' : 'Decimal value',
+'tax_breakup' : {
+    'total_igst' : 'Decimal value',
+    'total_cgst' : 'Decimal value',
+    'total_sgst' : 'Decimal value',
+    'total_cess' : 'Decimal value',
+},
+'charges' : {
+    'shipping_charges' : 'Decimal value',
+    'packaging_charges' : 'Decimal value',
+    'insurance_charges' : 'Decimal value',
+},
+'header_discount_amount' : "Global invoice discount in decimal number",
+'tcs_details':{
+    'applicable' : "Tax Collected at Source applicability - Boolean",
+    'rate_percentage' : "Decimal value",
+    'amount' : " TCS Amount",
+},
+'rounding_adjustment' : "Decimal value",
+'grand_total' : 'Decimal value',
+'amount_in_words' : "Amount in words",
+},
+'logistics_details' : {
+    'transporter_name': "",
+    'vehicle_number': "e.g., KA-01-AB-1234",
+    'lr_number' : "Lorry Receipt No",
+    'mode_of_transport' : "e.g., Road, Air, Sea"
+},
+'compliance_markers': {
+    'irn_hash' : '64-char E-Invoice Hash (Mandatory for >5 Cr turnover)',
+    'e_way_bill_number' : 'Logistics compliance number',
+    'delivery_proof_present' : 'True if LR/POD/E-Way Bill is found - Boolean',
+    'delivery_proof_type' : "ewaybill, LR ,  POD - ENUM",
+    'signature_present' : 'True if signed - Boolean',
+    'is_digitally_signed' : " True if DSC is detected - Boolean"
+},
+'invoice_header_meta' : {
+    'supply_type' : "Defines the GST nature of the transaction. Critical for validating tax rates (e.g., SEZ supplies are often zero-rated) - B2B/ B2C/ SEZ/ Deemed Export/ Export - ENUM",
+    'place_of_supply' : "The destination state code and name where goods are consumed. Determines IGST vs. CGST/SGST logic. Format: [State Code]-[State Name] (e.g., 27-Maharashtra, 29-Karnataka)",
+    'reverse_charge_applicable' : " true if the Buyer is liable to pay the tax directly to the government (common for Goods Transport Agencies or Legal Services).",
+    'due_date' : "The specific date by which payment is expected. Format: YYYY-MM-DD (e.g., 2024-01-15)",
+    'payment_terms' : 'The textual description of payment conditions. e.g Net 30 Days, Immediate, 50% Advance',
+    'payment_instruction' : "Specific text instructions for the bank transfer. Example: Please quote invoice no in transfer remarks",
+},
+'additional_attributes': {
+    'remarks' : 'Any "Notes" or "Remarks" text found on the invoice (e.g., "Handle with care").',
+    'sales_person' : 'Name of the Sales Representative or Account Manager mentioned.',
+    'delivery_slot' : 'Specific delivery timing or location instructions (e.g., "Morning Slot", "Dock 4")',
+    'customer_notes' : ' Specific instructions provided by the buyer printed on the invoice.'
+}
+
+
+}
+
+def get_api_keys():
+    try:
+        openai_api = st.secrets.get("OPENAI_API_KEY", "")
+    except:
+        openai_api = ""
+    return openai_api
+
+def encode_image(uploaded_image):
+    if uploaded_image is not None:
+       byte_data = uploaded_image.getvalue()
+       return base64.b64encode(byte_data).decode('utf-8')
+    return None
+
+def update_gsheet(data_dict):
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    try:
+        df = conn.read(spreadsheet=st.secrets.get("spreadsheet", ""), ttl=5)
+    except (pd.errors.EmptyDataError, Exception):
+        df = pd.DataFrame()
+    new_row_df = pd.DataFrame([data_dict])
+    updated_df = pd.concat([df, new_row_df], ignore_index=True)
+    conn.update(spreadsheet=st.secrets.get("spreadsheet", ""), data=updated_df)
+                        
+def get_gsheet_data():
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    try:
+        df = conn.read(spreadsheet=st.secrets.get("spreadsheet", ""), ttl=5)
+    except (pd.errors.EmptyDataError, Exception):
+        df = pd.DataFrame()
+    return df
+def extract_with_openai(image_base64, model_name, api_key):
+    client = openai.OpenAI(api_key=api_key)
+    prompt = f"""Extract all information from this invoice image and return it as a JSON object with the following structure:{json.dumps(INVOICE_SCHEMA, indent=2)}
+    Extract all visible information accurately. If a field is not present, leave it empty or as 0.
+    IMPORTANT: output valid JSON only. Do not wrap the output in markdown code blocks (e.g. ```json ... ```). just raw json text.
+    """
+
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{image_base64}"
+                        }
+                    }
+                ],
+            }
+        ],
+        max_tokens=2000,
+    )
+    
+    model = response.model
+    return response.choices[0].message.content, model
+
+
+    
+        
+    
